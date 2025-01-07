f736573ffa38e398bce4b205b9759af06c29f66c
Kartikay Sharma <136084119+Kartikaysharma2004@users.noreply.github.com>
2025-01-07 20:07:59 +0530

    Project 3

---
+++ GSAP Project 3/README.md
@@ -0,0 +1,39 @@
+# GSAP Project 3
+An innovative real estate website designed with Tailwind CSS and powered by GSAP animations. This project emphasizes smooth transitions, dynamic scrolling effects, and an intuitive user interface. It highlights modern web development practices while creating a visually appealing experience tailored to the real estate industry.
+
+<div align="center">
+<h3>GSAP Project built with Tailwind CSS & GSAP</h5>
+</div>
+
+<p align="center">
+  <a href="https://gsaproject3.netlify.app/">
+    <img src="https://img.shields.io/static/v1?label=&message=View%20Demo&style=for-the-badge&color=black&logo=vercel" />
+  </a>
+</p>
+
+# Features 🌟
+- **Scroll-Triggered Animations:**
+A project showcasing dynamic animations triggered as elements scroll into view. This demonstrates the integration of GSAP with ScrollTrigger for a seamless, engaging user experience.
+
+- **Interactive Contact Information Component:**
+A contact information section with hover effects and responsive design, brought to life using GSAP's powerful animation capabilities and tailored transitions.
+
+- **Interactive Navigation:** Smooth transitions make exploration effortless.
+
+- **Focus on Real Estate:** Designed to enhance the online presence of property businesses.
+
+## Tech Stack 🛠️
+<a href="https://html.com/" target="_blank" rel="noreferrer"> <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" /> </a> <a href="https://greensock.com/gsap/" target="_blank" rel="noreferrer"> <img src="https://img.shields.io/badge/gsap-%2388CE02.svg?style=for-the-badge&logo=greensock&logoColor=white" alt="GSAP" /> </a> <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="Tailwind CSS" /> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="JavaScript" /> </a>
+
+
+## 🚀 About Me
+I'm a Passionate Frontend Developer with a flair for transforming creative visions into interactive and user-centric digital experiences.
+
+## Connect with me:
+<p align="left">
+<a href="https://www.linkedin.com/in/kartikay-sharma2004/" target="blank"><img align="center" src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" /></a>
+  <a href="mailto:kartikaysharmaa2004@gmail.com" target="blank"><img align="center" src="https://img.shields.io/badge/gmail-D14836.svg?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail" /></a>
+  <a href="https://t.me/kartikay2004" target="blank"><img align="center" src="https://img.shields.io/badge/telegram-2CA5E0.svg?style=for-the-badge&logo=telegram&logoColor=white" alt="Gmail" /></a>
+  <a href="https://www.hackerrank.com/profile/kartikaysharmaa1" target="blank"><img align="center" src="https://img.shields.io/badge/hackerrank-2EC866.svg?style=for-the-badge&logo=hackerrank&logoColor=white" alt="HackerRank" /></a>
+<a href="https://peerlist.io/kartikaysharma" target="blank"><img align="center" src="https://img.shields.io/badge/Peerlist-2EC866.svg?style=for-the-badge&logo=peerlist&logoColor=white" alt="Peerlist"  /></a>
+</p>
---
+++ GSAP Project 3/script.js
@@ -0,0 +1,172 @@
+var tl = gsap.timeline();
+gsap.registerPlugin(TextPlugin)
+var section = document.querySelector("body");
+var list = document.querySelector("ul")
+
+section.addEventListener("mousemove", function (e) {
+    gsap.to(".cursor", {
+
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.5,
+        ease: "power2.out"
+    })
+})
+function listanimation() {
+    list.addEventListener("mouseenter", function () {
+        gsap.to(".cursor", {
+            scale: 2,
+            duration: 0.5,
+            backgroundColor: "#28282867",
+        })
+    })
+
+    list.addEventListener("mouseleave", function () {
+        gsap.to(".cursor", {
+            scale: 1,
+            duration: 0.3,
+            backgroundColor: "",
+        })
+
+    })
+}
+
+listanimation();
+
+function wheelanimation() {
+    window.addEventListener("wheel", function (e) {
+        gsap.to(".marque", {
+            transform: e.deltaY > 0 ? "translateX(-200%)" : "translateX(100%)", // Ternary operator for condition
+            duration: 4,
+            repeat: -1, // Infinite repeat
+            ease: "none",
+        })
+    })
+}
+
+wheelanimation();
+
+function isMobile() {
+    return window.innerWidth <= 768;
+}
+
+if (isMobile()) {
+    // Set fallback content for small devices
+    document.querySelector(".h2").textContent = "The Unsocial Network";
+    document.querySelector(".pg").textContent =
+        "Smartphones and especially social media apps are so addictive, learn to take control of your behavior.";
+        alert("This website is optimized for desktop or laptop use. Some features may not work correctly on mobile devices.")
+}
+
+if (window.innerWidth > 768) {
+
+    tl.from(".title", {
+        opacity: 0,
+        x: -20,
+        duration: 0.8,
+    })
+        .from("li", {
+            opacity: 0,
+            y: -15,
+            duration: 0.8,
+            ease: "slow(0.7,0.7,false)",
+            stagger: 0.15,
+        })
+    tl.from(".navbtn", {
+        visibility: "hidden",
+        rotate: 360,
+        duration: 2,
+    })
+    tl.to(".h2", {
+        opacity: 1,
+        duration: 3,
+        ease: "none",
+        text: "The Unsocial Network"
+    })
+    tl.to(".pg", {
+        opacity: 1,
+        duration: 2,
+        ease: "none",
+        text: "Smartphones and especially social media apps are so addictive, learn to take control of your behavior."
+    })
+    tl.from(".btn", {
+        visibility: "hidden",
+        duration: 1.2,
+        rotate: 360,
+        y: -15
+    })
+    tl.from(".mainimg", {
+        opacity: 0,
+        duration: 1, // Faster entry for the team section
+        x: "-50%",
+        ease: "power3.out"
+    })
+    tl.from(".page3 .h2ofpage2", {
+        opacity: 1,
+        duration: 4,
+        ease: "none",
+        text: "Lorem ipsum dolor sit",
+        scrollTrigger: {
+            trigger: ".page3",
+            start: "top 80%",
+            end: "bottom 30%",
+            endTrigger: ".footer",
+            scrub: 6,
+
+        }
+    })
+        .from(".page3 .pgofpage2", {
+            opacity: 1,
+            duration: 2,
+            ease: "steps(12)",
+            text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente mollitia dolore at repudiandae laborum dicta",
+            scrollTrigger: {
+                trigger: ".page2",
+                start: "top 60%",
+                end: "bottom 40%",
+                scrub: 5,
+                // markers: true,
+                endTrigger: ".footer",
+                toggleActions: "none"
+            }
+        })
+        .from(".page3 .secondimg", {
+            opacity: 0,
+            x: "-180%",
+            ease: "sine.out",
+            scrollTrigger: {
+                trigger: ".page2",
+                start: "top 80%",
+                end: "bottom 20%",
+                scrub: 5,
+                toggleActions: "none"
+            }
+        })
+
+        // Footer Animation
+        .from("footer .text-center", {
+            opacity: 0,
+            y: 50,
+            duration: 1,
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: "footer",
+                start: "top 90%",
+                end: "top 80%",
+                scrub: true,
+            }
+        })
+        .from("footer .flex a", {
+            opacity: 0,
+            scale: 0.5,
+            duration: 0.5,
+            stagger: 0.2,
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: "footer",
+                start: "top 90%",
+                end: "top 80%",
+                scrub: 2,
+            }
+        })
+}
